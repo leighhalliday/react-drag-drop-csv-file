bd67aa83a25faa2d93e4081d665cb7f9891b790f
Leigh Halliday <leighhalliday@gmail.com>
2020-05-02 15:21:43 -0400

    Demo for drag n drop files in React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { parse } from "papaparse";
 
-function App() {
+export default function App() {
+  const [highlighted, setHighlighted] = React.useState(false);
+  const [contacts, setContacts] = React.useState([
+    { email: "fake@gmail.com", name: "Fake" },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="text-center text-4xl">Contact Import</h1>
+      <div
+        className={`p-6 my-2 mx-auto max-w-md border-2 ${
+          highlighted ? "border-green-600 bg-green-100" : "border-gray-600"
+        }`}
+        onDragEnter={() => {
+          setHighlighted(true);
+        }}
+        onDragLeave={() => {
+          setHighlighted(false);
+        }}
+        onDragOver={(e) => {
+          e.preventDefault();
+        }}
+        onDrop={(e) => {
+          e.preventDefault();
+          setHighlighted(false);
+
+          Array.from(e.dataTransfer.files)
+            .filter((file) => file.type === "text/csv")
+            .forEach(async (file) => {
+              const text = await file.text();
+              const result = parse(text, { header: true });
+              setContacts((existing) => [...existing, ...result.data]);
+            });
+        }}
+      >
+        DROP HERE
+      </div>
+
+      <ul>
+        {contacts.map((contact) => (
+          <li key={contact.email}>
+            <strong>{contact.name}</strong>: {contact.email}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
-
-export default App;
